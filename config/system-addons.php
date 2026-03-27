5d66f819165a644c2700df2f15520ea7be3fbab1
jdev3multimedia <jdev3.multimedia@gmail.com>
2026-03-27 13:23:24 +0530

    Initial clean commit

---
+++ config/system-addons.php
@@ -0,0 +1,56 @@
+<?php return array (
+  'admin_panel' => 
+  array (
+    'active' => '1',
+    'username' => 'TmdStudio',
+    'purchase_key' => '811b7097-20d7-411e-ae0c-0ad7beb857ed',
+    'software_id' => 'MzY3NzIxMTI=',
+    'domain' => 'portal.ferdillcart.com',
+    'software_type' => 'product',
+  ),
+  'vendor_panel' => 
+  array (
+    'active' => '0',
+    'username' => '',
+    'purchase_key' => '',
+    'software_id' => '',
+    'domain' => '',
+    'software_type' => 'addon',
+  ),
+  'user_app' => 
+  array (
+    'active' => '0',
+    'username' => '',
+    'purchase_key' => '',
+    'software_id' => '',
+    'domain' => '',
+    'software_type' => 'addon',
+  ),
+  'vendor_app' => 
+  array (
+    'active' => '1',
+    'username' => 'TmdStudio',
+    'purchase_key' => '6d24d843-536f-4202-84dc-2348c9e12fa0',
+    'software_id' => 'MzY3NzIxNzM=',
+    'domain' => 'portal.ferdillcart.com',
+    'software_type' => 'addon',
+  ),
+  'deliveryman_app' => 
+  array (
+    'active' => '1',
+    'username' => 'TmdStudio',
+    'purchase_key' => '194dabc8-1937-4c68-9ebb-da4ff97614b7',
+    'software_id' => 'MzY3NzIxNDg=',
+    'domain' => 'portal.ferdillcart.com',
+    'software_type' => 'addon',
+  ),
+  'react_web' => 
+  array (
+    'active' => '1',
+    'username' => '',
+    'purchase_key' => '',
+    'software_id' => 'NDUzNzAzNTE=',
+    'domain' => '',
+    'software_type' => 'addon',
+  ),
+);
